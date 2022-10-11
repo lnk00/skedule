3d5a52f751b0289b7773a05bb4c984515afa30d4
Damien Dumontet <damien.dumontet@protonmail.com>
2022-10-11 12:36:57 -0300

    setup route

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,16 +1,25 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './app/routes/app';
+import Login from './app/routes/login';
 
-import App from './app/app';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
